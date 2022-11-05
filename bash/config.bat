468e74e3e6c0e1d10b6428db1bc6ef03c1c2d1b8
drew.minkin <drew.minkin@divergence.one>
2022-11-05 12:27:26 -0500

    last checkin

--- bash/config.bat
+++ bash/config.bat
@@ -3,5 +3,6 @@ pip install -U "databricks-connect==10.4.*"
 conda activate streetsmartTableauRSpark
 @REM https://adb-4002832894225232.12.azuredatabricks.net/?o=4002832894225232#setting/clusters/1105-063159-7j9yi5hs/configuration
 @REM personal access token: ffffdapi764addac1cfb0498c99c860714c4b9e0-3
+@REM 3<jdk_version<15
 conda install -c anaconda openjdk
 databricks-connect configure
